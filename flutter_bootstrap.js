139488324626b4f5afb28f7500d6876d6c0dcb83
Guido Denzler <guido@denzler.me>
2024-06-10 01:49:21 +0100

    UI Tweaks

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "902467874"
+    serviceWorkerVersion: "2411778199"
   }
 });
